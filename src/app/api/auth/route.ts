798f40bcd83d267147f4dabc8a5fe3a83a0b0417
Hamna R <hamnaraeel@gmail.com>
2023-04-17 01:00:43 +0500

    auth

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -23,3 +23,20 @@
 //     throw new Error("Your token has expired.");
 //   }
 // };
+
+import { Pool } from "@neondatabase/serverless";
+import { v4 as uuidv4 } from "uuid";
+
+export async function POST(request: Request) {
+  const { clientName, clientEmail } = await request.json();
+  const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+
+  const query =
+    "INSERT INTO api_clients (clientName, clientEmail) VALUES ($1, $2)";
+  const values = [clientName, clientEmail];
+  const response = await pool.query(query, values);
+  const { rows } = response;
+  const token = rows[0].token;
+  return new Response(JSON.stringify({ token }));
+}
+export const runtime = "edge";
