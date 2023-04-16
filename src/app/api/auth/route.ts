c1b3a5e9ff746a8d431f9020c7cf185cbf360a5c
Hamna R <hamnaraeel@gmail.com>
2023-04-17 02:54:50 +0500

    auth 5

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -3,15 +3,15 @@ import jwt from "jsonwebtoken";
 
 const KEY = "hgfdhjfjkdsbfjgdsjgfdjhsgffdsfdsfd";
 // eslint-disable-next-line import/no-anonymous-default-export
-export default function (req: NextApiRequest, res: NextApiResponse) {
+export default function GET(req: NextApiRequest, res: NextApiResponse) {
   if (!req.body) {
     res.statusCode = 404;
     res.end("Error");
     return;
   }
 
-  const clientName = req.body.clientName;
-  const clientEmail = req.body.clientEmail;
+  const clientName = "req.body.clientName";
+  const clientEmail = "req.body.clientEmail";
   const query =
     "INSERT INTO api_clients (clientName, clientEmail) VALUES ($1, $2)";
   const values = [clientName, clientEmail];
