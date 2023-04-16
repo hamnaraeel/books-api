f9fd45e2775a44c4b275ac59a3685e15af2cac40
Hamna R <hamnaraeel@gmail.com>
2023-04-16 19:49:16 +0500

    return all order-test changes

--- src/app/api/api-clients/route.ts
+++ src/app/api/api-clients/route.ts
@@ -1,33 +1,14 @@
 import { Pool } from "@neondatabase/serverless";
-import { NextResponse } from "next/server";
 import { v4 as uuidv4 } from "uuid";
-
-// export async function POST(request: Request) {
-//   const { clientName, clientEmail } = await request.json();
-//   const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-//   const token = uuidv4(); // generate a unique token
-//   const query =
-//     "INSERT INTO api_clients (clientName, clientEmail) VALUES ($1, $2)";
-//   const values = [clientName, clientEmail];
-//   await pool.query(query, values);
-//   return new Response(JSON.stringify({ token }));
-// }
 
 export async function POST(request: Request) {
   const { clientName, clientEmail } = await request.json();
   const pool = new Pool({ connectionString: process.env.DATABASE_URL });
   const token = uuidv4(); // generate a unique token
   const query =
-    "INSERT INTO api_clients (clientName, clientEmail, token) VALUES ($1, $2, $3) RETURNING token";
-  const values = [clientName, clientEmail, token];
-  const { rows } = await pool.query(query, values);
-  const accessToken = rows[0].token;
-  const headers = {
-    "Content-Type": "application/json",
-    Authorization: `Bearer ${accessToken}`,
-  };
-  const body = { token: accessToken };
-  return new NextResponse(JSON.stringify(body), { headers });
+    "INSERT INTO api_clients (clientName, clientEmail) VALUES ($1, $2)";
+  const values = [clientName, clientEmail];
+  await pool.query(query, values);
+  return new Response(JSON.stringify({ token }));
 }
-
 export const runtime = "edge";
