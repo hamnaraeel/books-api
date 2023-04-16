82ee08cd914ebb7d5182bab176c7f48b80ce125c
Hamna R <hamnaraeel@gmail.com>
2023-04-17 01:19:19 +0500

    no middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -25,16 +25,16 @@
 //   matcher: ["/dashboard", "/login"],
 // };
 
-// middleware.ts
+// // middleware.ts
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-// This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL("/status", request.url));
-}
+// // This function can be marked `async` if using `await` inside
+// export function middleware(request: NextRequest) {
+//   return NextResponse.redirect(new URL("/status", request.url));
+// }
 
-// See "Matching Paths" below to learn more
-export const config = {
-  matcher: "/api/:path*",
-};
+// // See "Matching Paths" below to learn more
+// export const config = {
+//   matcher: "/api/:path*",
+// };
