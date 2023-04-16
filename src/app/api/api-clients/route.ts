a37e76f026b01512f2e84d07beee316afcf53b81
Hamna R <hamnaraeel@gmail.com>
2023-04-17 03:11:05 +0500

    api-client

--- src/app/api/api-clients/route.ts
+++ src/app/api/api-clients/route.ts
@@ -5,10 +5,16 @@ export async function POST(request: Request) {
   const { clientName, clientEmail } = await request.json();
   const pool = new Pool({ connectionString: process.env.DATABASE_URL });
   const token = uuidv4(); // generate a unique token
+  const clientId = uuidv4(); // generate a unique token
+
   const query =
     "INSERT INTO api_clients (clientName, clientEmail) VALUES ($1, $2)";
 
   const values = [clientName, clientEmail];
+  await pool.query(query, values);
+  const querytoken =
+    "INSERT INTO api_clients (apiClientId, accessToken) VALUES ($1, $2)";
+  const valuestoken = [clientId, token];
   await pool.query(query, values);
 
   return new Response(JSON.stringify({ token }));
