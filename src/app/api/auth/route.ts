fa07eb499afdb38577bf91654ff3f4811a91ffa0
Hamna R <hamnaraeel@gmail.com>
2023-04-17 02:34:46 +0500

    jwt token

---
+++ src/app/api/auth/route.ts
@@ -0,0 +1,22 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import jwt from "jsonwebtoken";
+
+const KEY = "hgfdhjfjkdsbfjgdsjgfdjhsgf";
+export default function (req: NextApiRequest, res: NextApiResponse) {
+  if (!req.body) {
+    res.statusCode = 404;
+    res.end("Error");
+    return;
+  }
+  const { clientName, clientEmail } = req.body;
+
+  res.json({
+    token: jwt.sign(
+      {
+        clientName,
+        clientEmail,
+      },
+      KEY
+    ),
+  });
+}
