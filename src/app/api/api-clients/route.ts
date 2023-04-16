5ed64f55d1176266bc3ac5d9d08d41413b842817
Hamna R <hamnaraeel@gmail.com>
2023-04-17 01:26:47 +0500

    api-client-2

--- src/app/api/api-clients/route.ts
+++ src/app/api/api-clients/route.ts
@@ -4,10 +4,10 @@ import { v4 as uuidv4 } from "uuid";
 export async function POST(request: Request) {
   const { clientName, clientEmail } = await request.json();
   const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-
-  const query =
-    "INSERT INTO api_clients (clientName, clientEmail, token) VALUES ($1, $2, $3) RETURNING token";
   const token = uuidv4(); // generate a unique token
+  const query =
+    "INSERT INTO api_clients (clientName, clientEmail) VALUES ($1, $2)";
+
   const values = [clientName, clientEmail];
   await pool.query(query, values);
 
