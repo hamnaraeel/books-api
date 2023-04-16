f7aa8b3f6eb66a904f888f3f7aacafb487c93006
Hamna R <hamnaraeel@gmail.com>
2023-04-16 19:53:41 +0500

    token retry

--- src/app/api/api-clients/route.ts
+++ src/app/api/api-clients/route.ts
@@ -4,11 +4,11 @@ import { v4 as uuidv4 } from "uuid";
 export async function POST(request: Request) {
   const { clientName, clientEmail } = await request.json();
   const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-  const token = uuidv4(); // generate a unique token
+
   const query =
     "INSERT INTO api_clients (clientName, clientEmail) VALUES ($1, $2)";
   const values = [clientName, clientEmail];
-  await pool.query(query, values);
+  const token = await pool.query(query, values);
   return new Response(JSON.stringify({ token }));
 }
 export const runtime = "edge";
