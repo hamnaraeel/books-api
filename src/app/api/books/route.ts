219a483ee0dc076039dc47429615e14f168de7cf
Hamna R <hamnaraeel@gmail.com>
2023-04-17 02:02:43 +0500

    some middleware code but in a comment

--- src/app/api/books/route.ts
+++ src/app/api/books/route.ts
@@ -1,4 +1,6 @@
 import { Pool } from "@neondatabase/serverless";
+import { NextApiRequest, NextApiResponse } from "next";
+// import validateApiKey from "@/middleware";
 
 export async function GET(request: Request) {
   const pool = new Pool({ connectionString: process.env.DATABASE_URL });
@@ -6,5 +8,16 @@ export async function GET(request: Request) {
   // event.waitUntil(pool.end());  // doesn't hold up the response
   return new Response(JSON.stringify(rows));
 }
+
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse
+// ) {
+//   // Call the middleware before the main handler
+//   validateApiKey(req, res, async () => {
+//     const books = await GET(req);
+//     res.status(200).json(books);
+//   });
+// }
 
 export const runtime = "edge";
--- src/middleware.ts
+++ src/middleware.ts
@@ -12,13 +12,40 @@
 //   matcher: "/api/:path*",
 // };
 
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+// import { NextResponse } from "next/server";
+// import type { NextRequest } from "next/server";
 
-export function middleware(request: NextRequest) {
-  return NextResponse.rewrite(new URL("/api/status", request.url));
+// export function middleware(request: NextRequest) {
+//   return NextResponse.rewrite(new URL("/api/status", request.url));
 
-  //   if (request.nextUrl.pathname.startsWith("/dashboard")) {
-  //     return NextResponse.rewrite(new URL("/dashboard/user", request.url));
-  //   }
-}
+// }
+// export const config = {
+//   matcher: "/api/:path*",
+// };
+
+// import { NextApiRequest, NextApiResponse } from "next";
+// import { Pool } from "@neondatabase/serverless";
+
+// const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+
+// export default async function validateApiKey(
+//   req: NextApiRequest,
+//   res: NextApiResponse,
+//   next: () => void
+// ) {
+//   const { api_key } = req.headers;
+//   const query = "SELECT * FROM api_keys WHERE key = $1";
+//   const values = [api_key];
+
+//   try {
+//     const { rows } = await pool.query(query, values);
+
+//     if (rows.length === 0) {
+//       res.status(401).json({ message: "Invalid API key" });
+//     } else {
+//       next();
+//     }
+//   } catch (error) {
+//     res.status(500).json({ message: "Internal server error" });
+//   }
+// }
