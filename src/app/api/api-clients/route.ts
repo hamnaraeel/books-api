eada408381a151da027d75a17f6cfd4408c24abb
Hamna R <hamnaraeel@gmail.com>
2023-04-17 03:48:47 +0500

    orders-post 3

--- src/app/api/api-clients/route.ts
+++ src/app/api/api-clients/route.ts
@@ -2,17 +2,30 @@ import { Pool } from "@neondatabase/serverless";
 import { v4 as uuidv4 } from "uuid";
 
 export async function POST(request: Request) {
-  const { clientName, clientEmail } = await request.json();
+  // const { clientName, clientEmail } = await request.json();
+  // const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+  // const token = uuidv4(); // generate a unique token
+  // const clientId = uuidv4(); // generate a unique token
+
+  // const query =
+  //   "INSERT INTO api_clients_token (accessToken, clientName, clientEmail) VALUES ($1, $2, $3)";
+
+  // const values = [token, clientName, clientEmail];
+  // await pool.query(query, values);
+
+  // return new Response(JSON.stringify({ token }));
+
+  const { bookId, customerName } = await request.json();
   const pool = new Pool({ connectionString: process.env.DATABASE_URL });
   const token = uuidv4(); // generate a unique token
-  const clientId = uuidv4(); // generate a unique token
+  const orderId = uuidv4(); // generate a unique token
 
   const query =
-    "INSERT INTO api_clients_token (accessToken, clientName, clientEmail) VALUES ($1, $2, $3)";
+    "INSERT INTO orders (id, bookId, customerName) VALUES ($1, $2, $3)";
 
-  const values = [token, clientName, clientEmail];
+  const values = [orderId, bookId, customerName];
   await pool.query(query, values);
 
-  return new Response(JSON.stringify({ token }));
+  return new Response(JSON.stringify({ orderId }));
 }
 export const runtime = "edge";
