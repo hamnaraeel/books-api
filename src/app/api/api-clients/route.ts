0ac31ea2642250a641ed1b13365710c1749d2708
Hamna R <hamnaraeel@gmail.com>
2023-04-17 00:03:24 +0500

    added JWT

--- src/app/api/api-clients/route.ts
+++ src/app/api/api-clients/route.ts
@@ -1,16 +1,29 @@
 import { Pool } from "@neondatabase/serverless";
 import { v4 as uuidv4 } from "uuid";
+import jwt from "jsonwebtoken";
 
 export async function POST(request: Request) {
   const { clientName, clientEmail } = await request.json();
   const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 
   const query =
-    "INSERT INTO api_clients (clientName, clientEmail) VALUES ($1, $2)";
+    "INSERT INTO api_clients (clientName, clientEmail) VALUES ($1, $2) RETURNING clientId";
   const values = [clientName, clientEmail];
   const response = await pool.query(query, values);
   const { rows } = response;
-  const token = rows[0].token;
+
+  if (!process.env.JWT_SECRET) {
+    throw new Error("JWT_SECRET environment variable is not defined");
+  }
+
+  // Generate JWT token
+  const token = jwt.sign(
+    { clientId: rows[0].clientId },
+    process.env.JWT_SECRET,
+    { expiresIn: "1h" }
+  );
+
   return new Response(JSON.stringify({ token }));
 }
+
 export const runtime = "edge";
