5a4b49ad9f79f2525356704ab4b4eb71ccdb08b2
Hamna R <hamnaraeel@gmail.com>
2023-04-17 01:36:10 +0500

    middleware - 4

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,26 @@
-// middleware.ts
+// // middleware.ts
+// import { NextResponse } from "next/server";
+// import type { NextRequest } from "next/server";
+
+// // This function can be marked `async` if using `await` inside
+// export function middleware(request: NextRequest) {
+//   return NextResponse.redirect(new URL("/api/status", request.url));
+// }
+
+// // See "Matching Paths" below to learn more
+// export const config = {
+//   matcher: "/api/:path*",
+// };
+
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-// This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL("/api/status", request.url));
-}
+  if (request.nextUrl.pathname.startsWith("/")) {
+    return NextResponse.rewrite(new URL("/api/status", request.url));
+  }
 
-// See "Matching Paths" below to learn more
-export const config = {
-  matcher: "/api/:path*",
-};
+  //   if (request.nextUrl.pathname.startsWith("/dashboard")) {
+  //     return NextResponse.rewrite(new URL("/dashboard/user", request.url));
+  //   }
+}
