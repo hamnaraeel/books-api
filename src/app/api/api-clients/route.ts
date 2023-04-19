f1a8b73af6375222ebfe427f6ad851923dc28f5f
Hamna R <hamnaraeel@gmail.com>
2023-04-19 11:32:25 +0500

    clients post

--- src/app/api/api-clients/route.ts
+++ src/app/api/api-clients/route.ts
@@ -1,18 +1,24 @@
 import { Pool } from "@neondatabase/serverless";
 import { v4 as uuidv4 } from "uuid";
+import Joi from "joi";
+import { setUserToken } from "@/lib/auth";
 
 export async function POST(request: Request) {
   const { clientName, clientEmail } = await request.json();
-  const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-  const token = uuidv4(); // generate a unique token
+  const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+  });
+  // const token = uuidv4(); // generate a unique token
+
   const clientId = uuidv4(); // generate a unique token
 
   const query =
-    "INSERT INTO api_clients_token (accessToken, clientName, clientEmail) VALUES ($1, $2, $3)";
+    "INSERT INTO api_clients (clientName, clientEmail) VALUES ($1, $2)";
 
-  const values = [token, clientName, clientEmail];
-  await pool.query(query, values);
-
+  const values = [clientName, clientEmail];
+  const { rows } = await pool.query(query, values);
+  const token = await setUserToken(rows[0]);
   return new Response(JSON.stringify({ token }));
 }
 export const runtime = "edge";
