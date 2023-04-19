bbbb1386e5286d2d453e3dab2b55c4f22ef47e89
Hamna R <hamnaraeel@gmail.com>
2023-04-19 07:33:36 +0500

    post order 8

--- src/app/api/orders/route.ts
+++ src/app/api/orders/route.ts
@@ -1,12 +1,13 @@
 import { Pool } from "@neondatabase/serverless";
+import { NextApiRequest, NextApiResponse } from "next";
 import { v4 as uuidv4 } from "uuid";
 
-// export async function GET(request: Request) {
-//   const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-//   const { rows } = await pool.query("SELECT * FROM orders");
-//   // event.waitUntil(pool.end());  // doesn't hold up the response
-//   return new Response(JSON.stringify(rows));
-// }
+export async function GET(request: Request) {
+  const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+  const { rows } = await pool.query("SELECT * FROM orders");
+  // event.waitUntil(pool.end());  // doesn't hold up the response
+  return new Response(JSON.stringify(rows));
+}
 
 // export async function POST(request: Request) {
 //   const { bookId, customerName } = await request.json();
@@ -19,36 +20,56 @@ import { v4 as uuidv4 } from "uuid";
 //   return new Response(JSON.stringify({ orderId }));
 // }
 
-export async function POST(request: Request) {
-  const { bookId, customerName } = await request.json();
+// export async function POST(request: Request) {
+//   const { bookId, customerName } = await request.json();
+//   const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+//   const bookQuery = "SELECT * FROM books WHERE id = $1";
+//   const bookResult = await pool.query(bookQuery, [bookId]);
+//   const book = bookResult.rows[0];
+//   const orderId = uuidv4();
+//   const created = true;
+//   const createdBy = customerName;
+//   const quantity = 1;
+//   const timestamp = Date.now();
+
+//   const query =
+//     "INSERT INTO orders (id, bookId, customerName, created, createdBy, quantity, timestamp) VALUES ($1, $2, $3, $4, $5, $6, $7)";
+//   const values = [
+//     orderId,
+//     book.id,
+//     customerName,
+//     created,
+//     createdBy,
+//     quantity,
+//     timestamp,
+//   ];
+//   await pool.query(query, values);
+//   return new Response(JSON.stringify({ orderId }));
+// }
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-  const bookQuery = "SELECT * FROM books WHERE id = $1";
-  const bookResult = await pool.query(bookQuery, [bookId]);
-  const book = bookResult.rows[0];
-  const orderId = uuidv4();
-  const created = true;
-  const createdBy = customerName;
-  const quantity = 1;
-  const timestamp = Date.now();
+  if (req.method !== "POST") {
+    return res.status(405).json({ message: "Method Not Allowed" });
+  }
 
-  const query =
-    "INSERT INTO orders (id, bookId, customerName, created, createdBy, quantity, timestamp) VALUES ($1, $2, $3, $4, $5, $6, $7)";
-  const values = [
-    orderId,
-    book.id,
-    customerName,
-    created,
-    createdBy,
-    quantity,
-    timestamp,
-  ];
-  // const query =
-  //   "INSERT INTO orders (id, bookId, customerName) VALUES ($1, $2, $3) RETURNING id, bookId, customerName";
+  try {
+    const { bookId, customerName } = req.body;
 
-  // const values = [orderId, bookId, customerName];
-  await pool.query(query, values);
-  // const { id, bookid, customername } = result.rows[0];
-  return new Response(JSON.stringify({ orderId }));
+    // Insert a new order into the database
+    const result = await pool.query(
+      "INSERT INTO orders (bookId, customerName, created, createdBy, quantity, timestamp) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
+      [bookId, customerName, false, "", 0, Date.now()]
+    );
+
+    res.status(201).json({ order: result.rows[0] });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 }
 
 // export async function POST(request: Request) {
