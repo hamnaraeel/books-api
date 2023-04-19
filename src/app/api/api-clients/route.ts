e8cb8322e3995c80d4511ae01242617898142fc4
Hamna R <hamnaraeel@gmail.com>
2023-04-19 15:37:37 +0500

    undo all

--- src/app/api/api-clients/route.ts
+++ src/app/api/api-clients/route.ts
@@ -1,30 +1,18 @@
 import { Pool } from "@neondatabase/serverless";
 import { v4 as uuidv4 } from "uuid";
-// import { setUserToken } from "@/lib/auth";
+import { setUserToken } from "@/lib/auth";
 
 export async function POST(request: Request) {
-  try {
-    const { clientName, clientEmail } = await request.json();
-    const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-    const token = uuidv4(); // generate a unique token
-    const clientId = uuidv4(); // generate a unique token
+  const { clientName, clientEmail } = await request.json();
+  const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+  const token = uuidv4(); // generate a unique token
+  const clientId = uuidv4(); // generate a unique token
 
-    const query =
-      "INSERT INTO api_clients_token (accessToken, clientName, clientEmail) VALUES ($1, $2, $3)";
-    const values = [token, clientName, clientEmail];
-    const { rows } = await pool.query(query, values);
-    // const userToken = await setUserToken(rows[0]);
-    return new Response(JSON.stringify({ token, rows }));
-  } catch (error: any) {
-    return new Response(
-      JSON.stringify({
-        message: error.message.includes(
-          "duplicate key value violates unique constraint"
-        )
-          ? "User already created try with different email"
-          : error.message,
-      })
-    );
-  }
+  const query =
+    "INSERT INTO api_clients_token (accessToken, clientName, clientEmail) VALUES ($1, $2, $3)";
+  const values = [token, clientName, clientEmail];
+  const { rows } = await pool.query(query, values);
+  const userToken = await setUserToken(rows[0]);
+  return new Response(JSON.stringify({ token, userToken, rows }));
 }
 export const runtime = "edge";
--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -1,23 +1,23 @@
-// export const USER_TOKEN = "user-token";
-// import { Pool } from "@neondatabase/serverless";
+export const USER_TOKEN = "user-token";
+import { Pool } from "@neondatabase/serverless";
 
-// const JWT_SECRET_KEY: string | undefined = process.env.JWT_SECRET_KEY!;
+const JWT_SECRET_KEY: string | undefined = process.env.JWT_SECRET_KEY!;
 
-// export function getJwtSecretKey(): string {
-//   if (!JWT_SECRET_KEY || JWT_SECRET_KEY.length === 0) {
-//     throw new Error("The environment variable JWT_SECRET_KEY is not set.");
+export function getJwtSecretKey(): string {
+  if (!JWT_SECRET_KEY || JWT_SECRET_KEY.length === 0) {
+    throw new Error("The environment variable JWT_SECRET_KEY is not set.");
+  }
+  return JWT_SECRET_KEY;
+}
+
+// export async function getUserById(id: string) {
+//   const pool = new Pool({
+//     connectionString: process.env.DATABASE_URL,
+//   });
+//   try {
+//     const { rows } = await pool.query(`SELECT * FROM user WHERE id = ${id}`);
+//     return new Response(JSON.stringify({ rows }));
+//   } catch (err) {
+//     throw err;
 //   }
-//   return JWT_SECRET_KEY;
 // }
-
-// // export async function getUserById(id: string) {
-// //   const pool = new Pool({
-// //     connectionString: process.env.DATABASE_URL,
-// //   });
-// //   try {
-// //     const { rows } = await pool.query(`SELECT * FROM user WHERE id = ${id}`);
-// //     return new Response(JSON.stringify({ rows }));
-// //   } catch (err) {
-// //     throw err;
-// //   }
-// // }
