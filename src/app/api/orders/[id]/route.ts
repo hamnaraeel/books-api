021aa035d5399019770eeca7a32a1087786648b9
Hamna R <hamnaraeel@gmail.com>
2023-04-19 09:35:51 +0500

    delete order id

--- src/app/api/orders/[id]/route.ts
+++ src/app/api/orders/[id]/route.ts
@@ -17,40 +17,40 @@ export async function GET(
   return new Response(JSON.stringify(rows[0]));
 }
 
-// export async function PUT(
-//   request: Request,
-//   {
-//     params,
-//   }: {
-//     params: { id: string };
-//   }
-// ) {
-//   const { customerName } = await request.json();
-//   const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-//   const { rows } = await pool.query(
-//     `UPDATE orders SET customername = $1 WHERE id = $2 RETURNING *`,
-//     [customerName, params.id]
-//   );
-//   console.log(rows);
-//   // event.waitUntil(pool.end());  // doesn't hold up the response
-//   return new Response(JSON.stringify(rows[0]));
-// }
+export async function PUT(
+  request: Request,
+  {
+    params,
+  }: {
+    params: { id: string };
+  }
+) {
+  const { customerName } = await request.json();
+  const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+  const { rows } = await pool.query(
+    `UPDATE orders SET customername = $1 WHERE id = $2 RETURNING *`,
+    [customerName, params.id]
+  );
+  console.log(rows);
+  // event.waitUntil(pool.end());  // doesn't hold up the response
+  return new Response(JSON.stringify(rows[0]));
+}
 
-// export async function DELETE(
-//   request: Request,
-//   {
-//     params,
-//   }: {
-//     params: { id: string };
-//   }
-// ) {
-//   const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-//   const { rows } = await pool.query(
-//     `DELETE FROM books WHERE id = ${params.id} RETURNING *`
-//   );
-//   console.log(rows);
-//   // event.waitUntil(pool.end());  // doesn't hold up the response
-//   return new Response(JSON.stringify(rows[0]));
-// }
+export async function DELETE(
+  request: Request,
+  {
+    params,
+  }: {
+    params: { id: string };
+  }
+) {
+  const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+  const { rows } = await pool.query(
+    `DELETE FROM books WHERE id = ${params.id} RETURNING *`
+  );
+  console.log(rows);
+  // event.waitUntil(pool.end());  // doesn't hold up the response
+  return new Response(JSON.stringify(rows[0]));
+}
 
 export const runtime = "edge";
