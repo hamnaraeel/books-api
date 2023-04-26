5c8813060f80c8598d2ba28de6ff191616425c56
Hamna R <hamnaraeel@gmail.com>
2023-04-26 14:03:53 +0500

    api - clients

--- src/app/api/api-clients/route.ts
+++ src/app/api/api-clients/route.ts
@@ -11,9 +11,9 @@ export async function POST(request: Request) {
   const query =
     "INSERT INTO api_clients_token (accessToken, clientName, clientEmail) VALUES ($1, $2, $3)";
   const values = [token, clientName, clientEmail];
-  const { rows } = await pool.query(query, values);
+  const { rows }: any = await pool.query(query, values);
 
-  const userToken = await setUserToken(rows[0]);
+  const userToken = await setUserToken(token);
   return new Response(JSON.stringify({ token, userToken, rows }));
 }
 export const runtime = "edge";
